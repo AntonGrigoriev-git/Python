6132ab8ddae60a6f244bf0f3f7d0866ab6beaf7f
Anton Grigoriev <anton0806n@mail.ru>
2024-11-08 15:45:55 +0700

    Abstract Factory Builder

--- Abstract Factory Builder [05.11.24]/Digital goods store.py
+++ Abstract Factory Builder [05.11.24]/Digital goods store.py
@@ -1,153 +1,121 @@
 from abc import ABC, abstractmethod
 
-# Интерфейсы для классов Game и Music
-class Detailed(ABC):
+# Абстрактные продукты
+class Game(ABC):
     @abstractmethod
-    def get_details(self):
+    def get_details(self) -> str:
         pass
 
-class Downloadable(ABC):
     @abstractmethod
-    def download(self):
+    def download(self) -> str:
         pass
 
-class Playable(ABC):
     @abstractmethod
-    def play(self):
+    def play(self) -> str:
         pass
 
 
-# Абстрактные классы продуктов
-class Game(Detailed, Downloadable, Playable):
-    pass
+class Music(ABC):
+    @abstractmethod
+    def get_details(self) -> str:
+        pass
 
-class Music(Detailed, Downloadable, Playable):
-    pass
+    @abstractmethod
+    def download(self) -> str:
+        pass
 
+    @abstractmethod
+    def play(self) -> str:
+        pass
 
-# Конкретные классы для игр
+# Конкретные продукты для Game
 class ActionGame(Game):
-    def get_details(self):
-        print('Action Game: Dynamic and addictive game')
+    def get_details(self) -> str:
+        return 'Action Game: Dynamic and addictive game'
 
-    def download(self):
-        print('Downloading Action Game')
+    def download(self) -> str:
+        return 'Downloading Action Game'
 
-    def play(self):
-        print('Starting Action Game')
+    def play(self) -> str:
+        return 'Starting Action Game'
+
 
 class PuzzleGame(Game):
-    def get_details(self):
-        print('Pazzle Game: Fascinating puzzle for the mind')
+    def get_details(self) -> str:
+        return 'Puzzle Game: Fascinating puzzle for the mind'
 
-    def download(self):
-        print('Downloading Pazzle Game')
+    def download(self) -> str:
+        return 'Downloading Puzzle Game'
 
-    def play(self):
-        print('Starting Pazzle Game')
+    def play(self) -> str:
+        return 'Starting Puzzle Game'
 
-# Конкретные классы длля музыки
+# Конкретные продукты для Music
 class RockMusic(Music):
-    def get_details(self):
-        print('Rock Music: Energetic rock compositions')
+    def get_details(self) -> str:
+        return 'Rock Music: Energetic rock compositions'
 
-    def download(self):
-        print('Downloading Rock Music')
+    def download(self) -> str:
+        return 'Downloading Rock Music'
 
-    def play(self):
-        print('Playing Rock Music')
+    def play(self) -> str:
+        return 'Playing Rock Music'
+
 
 class JazzMusic(Music):
-    def get_details(self):
-        print('Jazz Music: Calm jazz melodies')
+    def get_details(self) -> str:
+        return 'Jazz Music: Calm jazz melodies'
 
-    def download(self):
-        print('Downloading Jazz Music')
+    def download(self) -> str:
+        return 'Downloading Jazz Music'
 
-    def play(self):
-        print('Playing Jazz Music')
+    def play(self) -> str:
+        return 'Playing Jazz Music'
 
-
-# Абстрактная фабрика для игр
-class GameFactory(ABC):
+# Абстрактная фабрика
+class DigitalProductFactory(ABC):
     @abstractmethod
     def create_game(self) -> Game:
         pass
 
-# Абстрактная фабрика для музыки
-class MusicFactory(ABC):
     @abstractmethod
     def create_music(self) -> Music:
         pass
 
-
-# Конкретные фабрики для игр
-class ActionGameFactory(GameFactory):
+# Конкретные фабрики
+class EntertainmentFactory(DigitalProductFactory):
     def create_game(self) -> Game:
         return ActionGame()
 
-class PuzzleGameFactory(GameFactory):
-    def create_game(self) -> Game:
-        return PuzzleGame()
-
-# Конкретные фабрики для музыки
-class RockMusicFactory(MusicFactory):
     def create_music(self) -> Music:
         return RockMusic()
 
-class JazzMusicFactory(MusicFactory):
+
+class PuzzleFactory(DigitalProductFactory):
+    def create_game(self) -> Game:
+        return PuzzleGame()
+
     def create_music(self) -> Music:
         return JazzMusic()
 
-
-# Абстрактная фабрика, которая определяет методы для получения фабрик типов продуктов
-class DigitalProductFactory(ABC):
-    @abstractmethod
-    def get_game_factory(self) -> GameFactory:
-        pass
-
-    @abstractmethod
-    def get_music_factory(self) -> MusicFactory:
-        pass
-
-
-# Конкретная фабрика развлечений, создающая ActionGame и RockMusic
-class EntertainmentFactory(DigitalProductFactory):
-    def get_game_factory(self) -> GameFactory:
-        return ActionGameFactory()
-
-    def get_music_factory(self) -> MusicFactory:
-        return RockMusicFactory()
-
-# Конкретная фабрика головоломок, создающая PuzzleGame и JazzMusic
-class PuzzleFactory(DigitalProductFactory):
-    def get_game_factory(self) -> GameFactory:
-        return PuzzleGameFactory()
-
-    def get_music_factory(self) -> MusicFactory:
-        return JazzMusicFactory()
-
-
+# Клиентский код
 def main(factory: DigitalProductFactory):
-    game_factory = factory.get_game_factory()
-    music_factory = factory.get_music_factory()
-
-    game = game_factory.create_game()
-    music = music_factory.create_music()
+    game = factory.create_game()
+    music = factory.create_music()
 
     products = [game, music]
 
     for item in products:
-        item.get_details()
-        item.download()
-        item.play()
+        print(item.get_details())
+        print(item.download())
+        print(item.play())
         print('-' * 40)
 
+if __name__ == "__main__":
+    print('Using EntertainmentFactory:')
+    entertainment_factory = EntertainmentFactory()
+    main(entertainment_factory)
 
-print('Using EntertainmentFactory:')
-entertainment_factory = EntertainmentFactory()
-main(entertainment_factory)
-
-print('\nUsing PuzzleFactory:')
-puzzle_factory = PuzzleFactory()
-main(puzzle_factory)
+    print('\nUsing PuzzleFactory:')
+    puzzle_factory = PuzzleFactory()
+    main(puzzle_factory)
